cb18881322558e0f50b5026a194f541df290277a
mariah johnson <mariahdawn9@gmail.com>
2022-10-09 08:30:58 +0000

    added in ingredients, equipment and direcitons html files, copied and added  AJAX function from demo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,34 @@
+//AJAX function starts here loads file into 
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML = this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
+
+
 //onload function and contents added form my "interactive recipe codepen https://codepen.io/mariahdawn9/pen/XWqqPMK 
 
 window.onload = function(){
+  
+loadFileInto("ingredients.html", "#ingredients");
 
 document.querySelector("#header h1").classList.add("makeBig"); //changes title on page  ``
   
@@ -24,4 +52,6 @@ document.querySelector("#ingredients h4").onclick = function() {
  
  document.querySelector("#ingredients ul").innerHTML +="<li>Love!</li>";
 }
+
+
 //end of window on load
